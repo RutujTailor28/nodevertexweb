18b438ae0ec4dcca3c9f638cf2ca796acdc0ae5c
RutujTailors <nisargtailor21@gmail.com>
2020-10-18 16:00:43 +0530

    Updates

---
+++ precache-manifest.eab002a717a362976689d06b39138bf4.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a04d7af405ce432e9f2dcd5601ccf56",
+    "url": "/nodevertexweb/index.html"
+  },
+  {
+    "revision": "4089c34aee28fc6b835c",
+    "url": "/nodevertexweb/static/css/2.9c71c652.chunk.css"
+  },
+  {
+    "revision": "a17770ddd4f76178bfff",
+    "url": "/nodevertexweb/static/css/main.00770f9f.chunk.css"
+  },
+  {
+    "revision": "4089c34aee28fc6b835c",
+    "url": "/nodevertexweb/static/js/2.928d3015.chunk.js"
+  },
+  {
+    "revision": "a8b7f9925879c67c458b78e4fa827b7f",
+    "url": "/nodevertexweb/static/js/2.928d3015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a17770ddd4f76178bfff",
+    "url": "/nodevertexweb/static/js/main.962d631a.chunk.js"
+  },
+  {
+    "revision": "b2e4cc217ee438e9d84e",
+    "url": "/nodevertexweb/static/js/runtime-main.dc69de38.js"
+  },
+  {
+    "revision": "348807c7ecb7270d15fccff16ffe3fe4",
+    "url": "/nodevertexweb/static/media/arrow-right-icon.348807c7.svg"
+  },
+  {
+    "revision": "8aa459ae951030ec44cb20c20de623c0",
+    "url": "/nodevertexweb/static/media/briefcase.8aa459ae.svg"
+  },
+  {
+    "revision": "5727f869509c87f4d25c2225f0968573",
+    "url": "/nodevertexweb/static/media/chevron-left.5727f869.svg"
+  },
+  {
+    "revision": "9c59704b1de34c9fbb54c100b907ac85",
+    "url": "/nodevertexweb/static/media/chevron-right.9c59704b.svg"
+  },
+  {
+    "revision": "ea3f87d138d7aecf39f43afa7707da47",
+    "url": "/nodevertexweb/static/media/clock.ea3f87d1.svg"
+  },
+  {
+    "revision": "6e80729ab3496e4871f5f183b0b4c9c7",
+    "url": "/nodevertexweb/static/media/customer-support-illustration.6e80729a.svg"
+  },
+  {
+    "revision": "7c1a6f97bf87de69a244342e6c17389c",
+    "url": "/nodevertexweb/static/media/customize-icon.7c1a6f97.svg"
+  },
+  {
+    "revision": "cbeab2705cd1a8c5d774eac8ed9c9619",
+    "url": "/nodevertexweb/static/media/dollar-sign.cbeab270.svg"
+  },
+  {
+    "revision": "1c251c8ff2222207650b4f24a2159cbb",
+    "url": "/nodevertexweb/static/media/dot-pattern.1c251c8f.svg"
+  },
+  {
+    "revision": "a1ad24916d979e088c5e8c80b073f92b",
+    "url": "/nodevertexweb/static/media/email-illustration.a1ad2491.svg"
+  },
+  {
+    "revision": "5e030c93ee64514b9fcdc887289ce8a9",
+    "url": "/nodevertexweb/static/media/facebook-icon.5e030c93.svg"
+  },
+  {
+    "revision": "6c9e80eec6977f6497b0e55f0fce374a",
+    "url": "/nodevertexweb/static/media/fast-icon.6c9e80ee.svg"
+  },
+  {
+    "revision": "f9776f55173cd40b2b3f3fed1c7443c4",
+    "url": "/nodevertexweb/static/media/logo.f9776f55.svg"
+  },
+  {
+    "revision": "b2f804bda31dee415fc2b1ff7ba9315f",
+    "url": "/nodevertexweb/static/media/map-pin.b2f804bd.svg"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/nodevertexweb/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "9661ba0b1e7abb32b6b4b956d5d14899",
+    "url": "/nodevertexweb/static/media/minus.9661ba0b.svg"
+  },
+  {
+    "revision": "7c6c8212afc5044e4b60f59552e57e69",
+    "url": "/nodevertexweb/static/media/plus.7c6c8212.svg"
+  },
+  {
+    "revision": "1de1406c7d731eac5d1fe0c638eab549",
+    "url": "/nodevertexweb/static/media/quotes-l.1de1406c.svg"
+  },
+  {
+    "revision": "1b5068d7d77725183060ce7dd9d6603c",
+    "url": "/nodevertexweb/static/media/quotes-r.1b5068d7.svg"
+  },
+  {
+    "revision": "a12572bdffec3f4a0b1df5e8a332a139",
+    "url": "/nodevertexweb/static/media/reliable-icon.a12572bd.svg"
+  },
+  {
+    "revision": "297d50a2cd8b864f0c1f357e5108d87a",
+    "url": "/nodevertexweb/static/media/shield-icon.297d50a2.svg"
+  },
+  {
+    "revision": "345871b443e4f5f841bedccf11a61859",
+    "url": "/nodevertexweb/static/media/simple-icon.345871b4.svg"
+  },
+  {
+    "revision": "a200bad45b923b0b206917b16755f83f",
+    "url": "/nodevertexweb/static/media/stats-illustration.a200bad4.svg"
+  },
+  {
+    "revision": "135d403ccd52c50bf2e773a8a68cea57",
+    "url": "/nodevertexweb/static/media/support-icon.135d403c.svg"
+  },
+  {
+    "revision": "ec52da39f670db5b4b3c32f54914a919",
+    "url": "/nodevertexweb/static/media/svg-decorator-blob-1.ec52da39.svg"
+  },
+  {
+    "revision": "159be2476eedd8b63bb0c7e0a58d0e47",
+    "url": "/nodevertexweb/static/media/svg-decorator-blob-3.159be247.svg"
+  },
+  {
+    "revision": "06b7e9297b931c8f3c729d9666faf31f",
+    "url": "/nodevertexweb/static/media/svg-decorator-blob-4.06b7e929.svg"
+  },
+  {
+    "revision": "50a3868db49539d53742411308690eba",
+    "url": "/nodevertexweb/static/media/svg-decorator-blob-5.50a3868d.svg"
+  },
+  {
+    "revision": "9317bc9dac095723b9f4afd2abaf7b15",
+    "url": "/nodevertexweb/static/media/tag.9317bc9d.svg"
+  },
+  {
+    "revision": "5ae3fe7a0d10e5c14ec0575db276dcda",
+    "url": "/nodevertexweb/static/media/team-illustration-2.5ae3fe7a.svg"
+  },
+  {
+    "revision": "7189052ca672d113f057d9a4f3cb5efe",
+    "url": "/nodevertexweb/static/media/twitter-icon.7189052c.svg"
+  },
+  {
+    "revision": "ed52f187b2c4aad7fd02765c9cdc2f92",
+    "url": "/nodevertexweb/static/media/user.ed52f187.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/nodevertexweb/static/media/x.b0200bf7.svg"
+  },
+  {
+    "revision": "45e500e6c6c6577b063b28adc39fb422",
+    "url": "/nodevertexweb/static/media/youtube-icon.45e500e6.svg"
+  }
+]);
