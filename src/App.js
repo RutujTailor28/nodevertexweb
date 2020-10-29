52306de9e06208c5297ee33c404951e76e7ede1d
RutujTailors <nisargtailor21@gmail.com>
2020-10-29 14:33:55 +0530

    comming-soon page added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import "tailwindcss/dist/base.css";
 import "styles/globalStyles.css";
 import React from "react";
-import { css } from "styled-components/macro"; //eslint-disable-line
 import AgencyLandingPage from "./demos/AgencyLandingPage";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import ComingSoon from "react-coming-soon";
+import CommingSoon from './assets/images/newcomming.jpg'
 export default function App() {
   return (
     <Router>
       <Switch>
         <Route path="/">
-          <AgencyLandingPage />
+          {/*<AgencyLandingPage />*/}
+            <ComingSoon
+                image={CommingSoon}
+                bgColor="#fff"
+                textColor="#fff"
+                illustration="development"
+                title="COMING SOON"
+            />
         </Route>
       </Switch>
     </Router>
