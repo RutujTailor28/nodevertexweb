8940130f297723a6cacb854f5a09f7f4d0f86e98
RutujTailors <nisargtailor21@gmail.com>
2020-11-24 17:45:35 +0530

    launching site phase - 1

--- src/components/testimonials/TwoColumnWithImageAndProfilePictureReview.js
+++ src/components/testimonials/TwoColumnWithImageAndProfilePictureReview.js
@@ -49,11 +49,11 @@ const Description = tw.p`max-w-md text-center mx-auto lg:mx-0 lg:text-left lg:ma
 
 const QuoteContainer = tw.div`relative mt-10 lg:mt-20`;
 const Quote = tw.blockquote`text-center lg:text-left text-sm sm:text-lg lg:text-xl xl:text-2xl`;
-const CustomerInfo = tw.div`mt-6 flex flex-col sm:flex-row items-center justify-center lg:justify-start`;
-const CustomerProfilePicture = tw.img`rounded-full w-20 h-20`;
-const CustomerTextInfo = tw.div`text-center lg:text-left sm:ml-6 mt-2 sm:mt-0`;
-const CustomerName = tw.h5`font-semibold text-xl lg:text-2xl xl:text-3xl text-primary-500`;
-const CustomerTitle = tw.p`font-medium text-secondary-100`;
+// const CustomerInfo = tw.div`mt-6 flex flex-col sm:flex-row items-center justify-center lg:justify-start`;
+// const CustomerProfilePicture = tw.img`rounded-full w-20 h-20`;
+// const CustomerTextInfo = tw.div`text-center lg:text-left sm:ml-6 mt-2 sm:mt-0`;
+// const CustomerName = tw.h5`font-semibold text-xl lg:text-2xl xl:text-3xl text-primary-500`;
+// const CustomerTitle = tw.p`font-medium text-secondary-100`;
 
 const QuotesLeft = tw(QuotesLeftIcon)`w-6 h-6 opacity-75 text-primary-500 inline-block mr-1 -mt-3`;
 const QuotesRight = tw(QuotesRightIcon)`w-6 h-6 opacity-75 text-primary-500 inline-block ml-1 -mt-3`;
--- src/demos/AgencyLandingPage.js
+++ src/demos/AgencyLandingPage.js
@@ -7,12 +7,12 @@ import Hero from "components/hero/BackgroundAsImage.js";
 import Features from "components/features/DashedBorderSixFeatures";
 import MainFeature from "components/features/TwoColSingleFeatureWithStats2.js";
 import MainFeature2 from "components/features/TwoColWithTwoFeaturesAndButtons.js";
-import Portfolio from "components/cards/PortfolioTwoCardsWithImage.js";
+// import Portfolio from "components/cards/PortfolioTwoCardsWithImage.js";
 import Blog from "components/blogs/ThreeColSimpleWithImageAndDashedBorder.js";
-import FAQ from "components/faqs/SimpleWithSideImage.js";
+// import FAQ from "components/faqs/SimpleWithSideImage.js";
 import ContactUsForm from "components/forms/TwoColContactUsWithIllustration.js";
 import Footer from "components/footers/MiniCenteredFooter.js";
-import customerSupportIllustrationSrc from "images/customer-support-illustration.svg";
+// import customerSupportIllustrationSrc from "images/customer-support-illustration.svg";
 import Technologies from "./Technologies";
 export default () => (
   <AnimationRevealPage>
